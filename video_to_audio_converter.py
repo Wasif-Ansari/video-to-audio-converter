b27b6cc4d6b1fcf6ad7bb5dc3a14dea06ad60506
Wasif Ansari <wasifansari2709@gmail.com>
2023-01-10 18:29:30 +0530

    converting video to audio

--- video_to_audio_converter.py
+++ video_to_audio_converter.py
@@ -4,3 +4,8 @@ from tkinter.filedialog import *
 vid = askopenfilename() #tkinter module to open a file
 
 video = moviepy.editor.VideoFileClip(vid) #to enable edit on vid file
+
+aud = video.audio
+aud.write_audiofile("demo.mp3")
+
+print(" ---END--- ")
