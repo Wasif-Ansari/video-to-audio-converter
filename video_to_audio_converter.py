0768c747bccd4bcbbdd7f1b82a7c7ca1a99798af
Wasif Ansari <wasifansari2709@gmail.com>
2023-01-10 18:28:46 +0530

    enable edit option on video file

--- video_to_audio_converter.py
+++ video_to_audio_converter.py
@@ -2,3 +2,5 @@ import moviepy.editor
 from tkinter.filedialog import *
 
 vid = askopenfilename() #tkinter module to open a file
+
+video = moviepy.editor.VideoFileClip(vid) #to enable edit on vid file
